f82d063c759dc42201e371c9987fa1ccf94c6749
Julian <julian.gierl@gmx.de>
2020-07-16 13:09:20 +0200

    Initial Commit

---
+++ GitHubFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 16.07.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
