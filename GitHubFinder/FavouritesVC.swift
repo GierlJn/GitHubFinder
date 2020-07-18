e58ba852867f3414762e24e8b2cb0da12b262b60
Julian <julian.gierl@gmx.de>
2020-07-18 11:31:31 +0200

    init

---
+++ GitHubFinder/FavouritesVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavouritesVC.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 16.07.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitHubFinder/GFButton.swift
@@ -0,0 +1,21 @@
+//
+//  GFButton.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 18.07.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
