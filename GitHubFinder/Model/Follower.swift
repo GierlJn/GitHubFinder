18e0aafcc7d49c882e38a2fad7d8501d75809422
Julian <julian.gierl@gmx.de>
2020-08-01 10:14:38 +0200

    Follower+

---
+++ GitHubFinder/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 31.07.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import Foundation
+
+
+struct Follower: Decodable{
+    var login: String
+    var avatarUrl: String
+}
