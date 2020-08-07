a609fcef89d8aa76794b5fe8ad83a55a48c1a5d6
Julian <julian.gierl@gmx.de>
2020-08-07 11:25:16 +0200

    Added loading animation for loading followers

--- GitHubFinder/Extensions/UIViewController+Ext.swift
+++ GitHubFinder/Extensions/UIViewController+Ext.swift
@@ -2,6 +2,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController{
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
@@ -10,6 +12,32 @@ extension UIViewController{
             alert.modalPresentationStyle = .overFullScreen
             alert.modalTransitionStyle = .crossDissolve
             self.present(alert, animated: true)
+        }
+    }
+    
+    func showLoadingView(){
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func hideLoadingView(){
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
