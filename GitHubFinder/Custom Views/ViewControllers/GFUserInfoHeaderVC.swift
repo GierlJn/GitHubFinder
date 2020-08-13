1af300b3790b15b014a5616667d51608777458f7
Julian <julian.gierl@gmx.de>
2020-08-13 09:04:30 +0200

    GFUserInfoHeaderVC+

---
+++ GitHubFinder/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,19 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 13.08.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
