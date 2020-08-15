abc1b2f709ae11468945e6116151cd3803a20bab
Julian <julian.gierl@gmx.de>
2020-08-15 12:25:12 +0200

    GFRepoItemVC+

---
+++ GitHubFinder/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFRepoItemVC.swift
+//  GitHubFinder
+//
+//  Created by Julian Gierl on 15.08.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configureItems(){
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+
+}
