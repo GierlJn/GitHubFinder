b0a0e0d6c9356407fea8d271c1c0d4ffa01d3c11
Julian <julian.gierl@gmx.de>
2020-08-24 13:50:49 +0200

    retrieve favourites from user defaults test

--- GitHubFinder/Screens/FavouritesVC.swift
+++ GitHubFinder/Screens/FavouritesVC.swift
@@ -1,10 +1,4 @@
-//
-//  FavouritesVC.swift
-//  GitHubFinder
-//
-//  Created by Julian Gierl on 16.07.20.
-//  Copyright © 2020 Julian Gierl. All rights reserved.
-//
+
 
 import UIKit
 
@@ -13,7 +7,16 @@ class FavouritesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let follower):
+                print(follower)
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
