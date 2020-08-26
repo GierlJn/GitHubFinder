11cb57503945389361d4821d19f8470550005a61
Julian <julian.gierl@gmx.de>
2020-08-26 19:09:51 +0200

    Refactored FavoritesListeVC

--- GitHubFinder/Screens/FavouritesVC.swift
+++ GitHubFinder/Screens/FavouritesVC.swift
@@ -6,8 +6,23 @@ class FavouritesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        getFavorites()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    func configureViewController(){
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
-        
+    }
+
+    func getFavorites() {
         PersistenceManager.retrieveFavorites { result in
             switch result {
             case .success(let follower):
@@ -16,13 +31,6 @@ class FavouritesVC: UIViewController {
                 print(error.rawValue)
             }
         }
-        
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = false
-    }
-
 
 }
