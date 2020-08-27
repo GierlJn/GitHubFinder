54b5e233a6b2351a701398da26dbaba2a661717d
Julian <julian.gierl@gmx.de>
2020-08-27 15:05:19 +0200

    Delete favorites

--- GitHubFinder/Screens/FavouritesVC.swift
+++ GitHubFinder/Screens/FavouritesVC.swift
@@ -76,7 +76,22 @@ extension FavouritesVC: UITableViewDelegate, UITableViewDataSource{
         destVC.username = favorite.login
         destVC.title = favorite.login
         navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
         
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            
+            guard let error = error else { return }
+            
+            self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+        }
     }
     
 }
