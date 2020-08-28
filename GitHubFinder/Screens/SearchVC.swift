365ce95f64017f9f09265bac526a26dedfdadfeb
Julian <julian.gierl@gmx.de>
2020-08-28 16:19:41 +0200

    Move up constraint for iPhone SE

--- GitHubFinder/Screens/SearchVC.swift
+++ GitHubFinder/Screens/SearchVC.swift
@@ -7,6 +7,7 @@ class SearchVC: UIViewController {
     let logoImageView = UIImageView()
     let callToActionButton = GFButton(title: "Get followers", backgroundColor: .systemGreen)
     let userInputTextfield = GFTextField()
+    var logoImageViewTopConstraint: NSLayoutConstraint!
     
     var isUserNameEntered:Bool { return !userInputTextfield.text!.isEmpty}
     
@@ -46,8 +47,13 @@ class SearchVC: UIViewController {
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.image = UIImage(named: "gh-logo")!
         
+        let topConstraintConstant: CGFloat = DeviceType.isiPhoneSE || DeviceType.isiPhone8Zoomed ? 20 : 80
+        
+        logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
+        
+        logoImageViewTopConstraint.isActive = true
+            
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
